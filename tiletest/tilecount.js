08daeeeb57491bcfbfb5332b6f16f1bcbd726691
AllenC123 <221612802+AllenC123@users.noreply.github.com>
2025-08-22 11:27:23 -0800

    refactored CSS and JS files more responsive mouse-interaction

--- tiletest/tilecount.js
+++ tiletest/tilecount.js
@@ -1,49 +1,32 @@
 VALUE_PER_TILE = 1;
+IS_TILE_GRABBED = false;
 
 
 function CheckScrollCountRequirement(element, current_value){
     req_count = element.getAttribute("required_scrollcount");
     if (current_value >= req_count) {
-        console.log("hit scrollcount!");
         element.removeAttribute("hidden");
     } else {
-        element.setAttribute("hidden",true);
+        element.setAttribute("hidden", true);
     }
     // TODO: actually adjust the position here
     return req_count;
 }
 
-
 // iterates through elements and matches their position to their scrollcount-threshold
-function AdjustAllThings(current_value) {
-    target_elements = document.getElementsByClassName("testme");
-    threshold_list = document.getElementById("threshold_list");
-    list_entries = threshold_list.getElementsByTagName("li");
-    while (list_entries.length < target_elements.length) {
-        new_entry = document.createElement("li");
-        new_entry.setAttribute("class","bad");
-        threshold_list.appendChild(new_entry);
-        console.log(list_entries);
-    }
-    
+function UpdateThresholdList(current_value) {
     I = 0;
     for (element of target_elements) {
-        r=CheckScrollCountRequirement(element, current_value);
+        CheckScrollCountRequirement(element, current_value);
         assoc_entry = list_entries[I++];
         if (current_value >= req_count)
             assoc_entry.setAttribute("class", "good");
         else assoc_entry.setAttribute("class", "bad");
-        assoc_entry.textContent = req_count;
     }
 }
 
-
-// update counters/progress when mousebutton is released (resize finished)
-window.addEventListener('mouseup', function() {
-    var counter = document.getElementById('counter');
-    var counted = document.getElementById('countme');
-    // these ^ must be set here, not in global scope (elements are undefined when script is imported)
-    
+function CounterUpdate() {
+    if (!IS_TILE_GRABBED) return;
     style = window.getComputedStyle(counted);
     width = style.getPropertyValue('width').replace('px',''); // need to remove 'px' suffix before math operations
     height = style.getPropertyValue('height').replace('px','');
@@ -55,7 +38,6 @@ window.addEventListener('mouseup', function() {
     row_count = Math.round(height / tile_height);
     tile_count = row_count * num_per_row;
     total_value = tile_count * VALUE_PER_TILE;
-    counter.innerHTML = total_value.toString() + '⚡kWh'; // unicode: U+26A1 'High Voltage Sign'
-    
-    AdjustAllThings(total_value);
-});
+    counter.innerHTML = total_value.toString() + '\u26A1kWh'; // unicode: U+26A1 'High Voltage Sign'
+    UpdateThresholdList(total_value);
+}
