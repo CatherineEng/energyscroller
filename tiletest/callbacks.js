ef98e2920fcbfdd84015e2749a0cbefbaa09c2c2
AllenC123 <221612802+AllenC123@users.noreply.github.com>
2025-08-25 14:46:20 -0800

    variable declarations made explicit

--- tiletest/callbacks.js
+++ tiletest/callbacks.js
@@ -1,30 +1,30 @@
 // import this script at the end of the HTML (after all elements are defined)
 
-var counter = document.getElementById('counter');
-var counted = document.getElementById('countme');
+const counter = document.getElementById('counter');
+const counted = document.getElementById('countme');
 
-var target_elements = document.getElementsByClassName("testme");
-var threshold_list = document.getElementById("threshold_list");
-var list_entries = threshold_list.getElementsByTagName("li");
+const target_elements = document.getElementsByClassName("testme");
+const threshold_list = document.getElementById("threshold_list");
+const list_entries = threshold_list.getElementsByTagName("li");
 
 function CreateThresholdEntries() {
     while (list_entries.length < target_elements.length) {
-        new_entry = document.createElement("li");
+        const new_entry = document.createElement("li");
         new_entry.setAttribute("class","bad");
         threshold_list.appendChild(new_entry);
         // console.log(list_entries);
     }
     
-    I = 0;
+    let I = 0;
     for (element of target_elements) {
-        assoc_entry = list_entries[I++];
-        threshold = element.getAttribute("required_scrollcount");
+        let assoc_entry = list_entries[I++];
+        let threshold = element.getAttribute("required_scrollcount");
         counterMap.set(element, new CounterEntry(threshold, assoc_entry));
     }
 }
 
 CreateThresholdEntries();
-console.log(counterMap);
+//console.log(counterMap);
 UpdateCounterMap();
 
 window.addEventListener('resize', UpdateCounter);
--- tiletest/tilecount.js
+++ tiletest/tilecount.js
@@ -1,4 +1,4 @@
-VALUE_PER_TILE = 1;
+let VALUE_PER_TILE = 1;
 
 const counterMap = new Map();
 class CounterEntry {
@@ -17,30 +17,30 @@ function UpdateCounterMap(counter_value) {
             entry.assoc_entry.setAttribute("class", "good");
         } else {
             element.setAttribute("hidden", true);
-            assoc_entry.setAttribute("class", "bad");
+            entry.assoc_entry.setAttribute("class", "bad");
         }
     }
 }
 
 function UpdateCounter() {
-    style = window.getComputedStyle(counted);
-    width = style.getPropertyValue('width').replace('px',''); // need to remove 'px' suffix before math operations
-    height = style.getPropertyValue('height').replace('px','');
-    tile_size = style.getPropertyValue('background-size'); // "auto 200px"
-    tile_height = String(tile_size).split(" ")[1].replace('px','');
+    let style = window.getComputedStyle(counted);
+    let width = style.getPropertyValue('width').replace('px',''); // need to remove 'px' suffix before math operations
+    let height = style.getPropertyValue('height').replace('px','');
+    let tile_size = style.getPropertyValue('background-size'); // "auto 200px"
+    let tile_height = String(tile_size).split(" ")[1].replace('px','');
     //tile_width = 1000*(tile_height/2000) // assuming that height is 200px, this will be 100px (scaled from 1000x2000)
-    tile_width = tile_height/2; // aspect ratio (1:2) should be preserved
+    let tile_width = tile_height/2; // aspect ratio (1:2) should be preserved
     
-    num_per_row = Math.round(width / tile_width);
-    row_count = Math.round(height / tile_height);
-    tile_count = row_count * num_per_row;
-    total_value = tile_count * VALUE_PER_TILE;
+    let num_per_row = Math.round(width / tile_width);
+    let row_count = Math.round(height / tile_height);
+    let tile_count = row_count * num_per_row;
+    let total_value = tile_count * VALUE_PER_TILE;
     counter.innerHTML = total_value.toString() + '\u26A1kWh'; // unicode: U+26A1 'High Voltage Sign'
     UpdateCounterMap(total_value);
     
-    var topbar_height = document.getElementById('topbar_info').clientHeight;
+    const topbar_height = document.getElementById('topbar_info').clientHeight;
     for (const element of target_elements) {
-        req_count = element.getAttribute("required_scrollcount");
+        let req_count = element.getAttribute("required_scrollcount");
         if (req_count > total_value) continue;
         element.style.top = topbar_height+((tile_height*req_count)/num_per_row) + "px";
     }
