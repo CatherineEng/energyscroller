d04d5d1877627b008bb4e6174f62b6c9a0e47333
AllenC123 <221612802+AllenC123@users.noreply.github.com>
2025-08-29 13:14:56 -0800

    Complete JS rewrite. Implemented multiple counters and counting all tiles

--- tiletest/callbacks.js
+++ tiletest/callbacks.js
@@ -1,40 +1,69 @@
+"use strict";
 // import this script at the end of the HTML (after all elements are defined)
 
-const counter = document.getElementById('counter');
-const counted = document.getElementById('countme');
-
-const tiling_things = document.getElementsByClassName("resizeable_tiling");
-const target_elements = document.getElementsByClassName("testme");
-const threshold_list = document.getElementById("threshold_list");
-const list_entries = threshold_list.getElementsByTagName("li");
-
-function CreateThresholdEntries() {
-    while (list_entries.length < target_elements.length) {
-        const new_entry = document.createElement("li");
-        new_entry.setAttribute("class","bad");
-        threshold_list.appendChild(new_entry);
-        // console.log(list_entries);
-    }
+function InitializeElementMap()
+{
+    const tile_sources = document.getElementsByClassName("resizeable_tiling");
+    console.log("tile sources: ", tile_sources);
     
-    let I = 0;
-    for (const element of target_elements) {
-        let assoc_entry = list_entries[I++];
-        let threshold = element.getAttribute("required_scrollcount");
-        counterMap.set(element, new CounterEntry(threshold, assoc_entry));
-    }
-    
-    // setting background-size if specified in HTML attribute
-    for (const thing of tiling_things) {
-        if (thing.getAttribute("tilesize")) {
-            console.log(`tilesize: ${thing.getAttribute("tilesize")}`);
-            thing.style.backgroundSize = thing.getAttribute("tilesize");
+    for (const tile_source of tile_sources) {
+        // setting background-size if specified in HTML attribute
+        if (tile_source.hasAttribute("tilesize")) {
+            let tilesize = tile_source.getAttribute("tilesize");
+            tile_source.style.backgroundSize = tilesize;
+            console.log(`specified tilesize: ${tilesize}`);
+        } else {
+            tile_source.style.backgroundSize = getComputedStyle(tile_source).getPropertyValue('background-size');
+            console.log(`default tilesize: ${tile_source.style.backgroundSize}`);
         }
+        
+        let tuplelist = [];
+        let targetlist = tile_source.getElementsByClassName("testme");
+        for (const target of targetlist) {
+            let threshold = target.getAttribute("required_scrollcount");
+            tuplelist.push([target, threshold]);
+        }
+        console.log("tuple_list: ", tuplelist);
+        ElementMap.set(tile_source, tuplelist);
+        TileValues.set(tile_source, 0);
+        
+        tile_source.addEventListener('mousemove', CalculateTileValue.bind(null, tile_source));
     }
 }
 
-CreateThresholdEntries();
-//console.log(counterMap);
-UpdateCounterMap();
+function ConstructCounterTables()
+{
+    const table_container = document.getElementById("table_container");
+    const tile_sources = document.getElementsByClassName("resizeable_tiling");
+    
+    let table_counter = 1;
+    for (const tile_source of tile_sources) {
+        const table = document.createElement("table");
+        table.setAttribute("class","threshold_table");
+        
+        let table_name = `Table_${table_counter++}`;
+        const header = document.createElement("th");
+        header.innerHTML = `<a class="table_counter">0\u26A1</a> ${table_name}`; // inline table-counter
+        // the counter MUST come before the name for proper layout
+        // escaped sequence is unicode: U+26A1 'High Voltage Sign'
+        
+        // for proper layout, the header must be nested inside a 'tbody' element,
+        // then 'li' elements (rows) inserted afterward (outside closing <tbody>)
+        const table_body = document.createElement("tbody");
+        table.appendChild(table_body);
+        table_body.appendChild(header);
+        table_container.appendChild(table);
+        
+        CounterMap.set(tile_source, new CounterTable(table, tile_source));
+    }
+}
 
-window.addEventListener('resize', UpdateCounter);
-counted.addEventListener('mousemove', UpdateCounter);
+
+InitializeElementMap();
+ConstructCounterTables();
+console.log("ElementMap:", ElementMap);
+console.log("CounterMap:", CounterMap);
+
+window.addEventListener('resize', UpdateAll);
+window.addEventListener('scroll', UpdateAll);
+UpdateAll();
--- tiletest/tilecount.js
+++ tiletest/tilecount.js
@@ -1,47 +1,124 @@
-let VALUE_PER_TILE = 1;
+"use strict"; // <-- strict mode
+// developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+/* 'this' inside local functions normally returns 'globalThis'.
+    however, within strict-mode it returns 'undefined' instead,
+    which allows us to determine whether strict-mode is active. */
+let isStrictMode = (function() { return (this !== globalThis); })();
+console.log(`strict-mode: ${(isStrictMode? 'enabled':'disabled')}`);
+// --------------------------------------------------------------- //
 
-const counterMap = new Map();
-class CounterEntry {
-    constructor(threshold, assoc_entry) {
-        this.threshold = threshold
-        this.assoc_entry = assoc_entry;
-        this.assoc_entry.textContent = this.threshold;
+let VALUE_PER_ICON = 1; // TODO: custom HTML attribute
+const TileValues = new Map(); // tile --> stored value
+const ElementMap = new Map(); // tile --> nested elements (class="testme")
+const CounterMap = new Map(); // tile --> CounterTable
+
+
+class TableEntry
+{
+    threshold;
+    table_row; // the <li> element associated with this
+    
+    Update(new_val) { // updates the coloring (via CSS)
+        this.table_row.setAttribute("class",
+              ((new_val >= this.threshold)? "good":"bad"));
+        return (new_val >= this.threshold);
+    }
+    
+    constructor(parentTable, target)
+    { // inserts an entry for target into the parent CounterTable
+        let threshold = target.getAttribute("required_scrollcount");
+        if (!target.hasAttribute("name"))
+            {target.setAttribute("name", target.innerText);}
+        let target_name = target.getAttribute("name");
+        
+        const new_entry = document.createElement("li");
+        new_entry.setAttribute("class", "bad");
+        new_entry.textContent = `[${threshold}] ${target_name}`
+        parentTable.appendChild(new_entry);
+        
+        this.threshold = threshold;
+        this.table_row = new_entry;
     }
 }
 
-function UpdateCounterMap(counter_value) {
-    for (const [element, entry] of counterMap.entries())
-    {
-        if (counter_value >= entry.threshold) {
-            element.removeAttribute("hidden");
-            entry.assoc_entry.setAttribute("class", "good");
-        } else {
-            element.setAttribute("hidden", true);
-            entry.assoc_entry.setAttribute("class", "bad");
+class CounterTable
+{
+    table_element; // <table class="threshold_table">
+    table_counter; // <a class="table_counter">
+    tile_source; // <class="resizeable_tiling">
+    target_list; // <class="testme"> elements nested within tile_source
+    row_entries; // TableEntries
+    
+    Update(new_val) {
+        this.table_counter.textContent = `${new_val}\u26A1`
+        for (const entry of this.row_entries) entry.Update(new_val);
+    }
+    
+    // table element needs to be pre-constructed
+    constructor(table, source) {
+        this.table_element = table;
+        this.table_counter = table.getElementsByClassName("table_counter")[0];
+        this.tile_source = source;
+        this.target_list = source.getElementsByClassName("testme");
+        
+        this.row_entries = [];
+        for (const target of this.target_list) {
+            this.row_entries.push(new TableEntry(this.table_element, target));
         }
     }
 }
 
-function UpdateCounter() {
-    let style = window.getComputedStyle(counted);
-    let width = style.getPropertyValue('width').replace('px',''); // need to remove 'px' suffix before math operations
-    let height = style.getPropertyValue('height').replace('px','');
-    let tile_size = style.getPropertyValue('background-size'); // "auto 200px"
-    let tile_height = String(tile_size).split(" ")[1].replace('px','');
-    //tile_width = 1000*(tile_height/2000) // assuming that height is 200px, this will be 100px (scaled from 1000x2000)
+function UpdateCounterTotal() {
+    let total_value = 0;
+    for (const val of TileValues.values()){ total_value += val; }
+    const primary_counter = document.getElementById('counter');
+    primary_counter.innerText = `${total_value}\u26A1kWh`; // unicode: U+26A1 'High Voltage Sign'
+    return total_value;
+}
+
+function CalculateTileValue(tile_element)
+{
+    //let prev_val = TileValues.get(tile_element);
+    let tile_val = 0;
+    
+    // DOMRect { x: 8, y: 386, width: 502, height: 202, top: 386, right: 510, bottom: 588, left: 8 }
+    let bounds = tile_element.getBoundingClientRect();
+    let tile_size = tile_element.style.backgroundSize; // "auto 200px"
+    let tile_height = tile_size.split(" ")[1].replace('px','');
+    //let tile_width = 1000*(tile_height/2000) // assuming that height is 200px, this will be 100px (scaled from 1000x2000)
     let tile_width = tile_height/2; // aspect ratio (1:2) should be preserved
     
-    let num_per_row = Math.round(width / tile_width);
-    let row_count = Math.round(height / tile_height);
-    let tile_count = row_count * num_per_row;
-    let total_value = tile_count * VALUE_PER_TILE;
-    counter.innerHTML = total_value.toString() + '\u26A1kWh'; // unicode: U+26A1 'High Voltage Sign'
-    UpdateCounterMap(total_value);
+    let num_per_row = Math.round(bounds.width / tile_width);
+    let row_count = Math.round(bounds.height / tile_height);
+    let icon_count = row_count * num_per_row;
+    tile_val = icon_count * VALUE_PER_ICON;
     
-    const topbar_height = document.getElementById('topbar_info').clientHeight;
-    for (const element of target_elements) {
-        let req_count = element.getAttribute("required_scrollcount");
-        if (req_count > total_value) continue;
-        element.style.top = topbar_height+((tile_height*req_count)/num_per_row) + "px";
+    //if (tile_val == prev_val) return tile_val;
+    TileValues.set(tile_element, tile_val);
+    CounterMap.get(tile_element).Update(tile_val);
+    UpdateCounterTotal();
+    
+    // adjusting for vertical scaling of the icons. (row_count+1) places it at the row instead of below
+    let height_ratio = (bounds.height/((row_count+1)*tile_height));
+    //console.log(`tile_height: ${tile_height} : ${height_ratio}`);
+    //console.log(`new_height: ${tile_height*height_ratio}`);
+    tile_height *= height_ratio;
+    
+    // TODO: still can't use relative positioning because they want to be relative to the text
+    let vertOffset = bounds.top + window.scrollY;
+    
+    // adjusting position of each child element
+    let elements = ElementMap.get(tile_element);
+    for (const [target, threshold] of elements) {
+        if (tile_val >= threshold) { target.removeAttribute("hidden");
+            target.style.top = `${vertOffset+((tile_height*threshold)/num_per_row)}px`
+        } else { target.setAttribute("hidden", true); }
     }
+    
+    return tile_val;
+}
+
+function UpdateAll() {
+    for (const tile of TileValues.keys()) { CalculateTileValue(tile); }
+    // return UpdateCounterTotal();
 }
